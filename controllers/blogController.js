87cb5cec3ae095b7cea52ae3fd5b60a601aa22b2
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-02-18 18:19:31 +0530

    Complete PUT request to update blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,6 +3,7 @@ const asyncHandler = require("express-async-handler");
 
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const utils = require("../config/utils")
 
 exports.home = asyncHandler(async (req, res, next) => {
     const firstFive = await Blog.find({}).limit(5)
@@ -22,10 +23,10 @@ exports.getList = asyncHandler(async (req, res, next) => {
 
 exports.getBlog = asyncHandler(async (req, res, next) => {
     const blog = await Blog.findById(req.params['blogId'])
-    if(!blog){
-        res.json({success: false , msg: "No blog found with that id"})
+    if (!blog) {
+        res.json({ success: false, msg: "No blog found with that id" })
     }
-    res.json({success: true , blog: blog})
+    res.json({ success: true, blog: blog })
 })
 
 exports.create = [
@@ -42,31 +43,63 @@ exports.create = [
     asyncHandler(async (req, res, next) => {
         const error = validationResult(req)
         if (!error.isEmpty()) {
-            res.json(error)
+            return res.json(error)
         }
-        const currentdate = new Date();
-        const datetime = currentdate.getDate() + "/"
-            + (currentdate.getMonth() + 1) + "/"
-            + currentdate.getFullYear() + " @ "
-            + currentdate.getHours() + ":"
-            + currentdate.getMinutes() + ":"
-            + currentdate.getSeconds();
-        const user = await User.findOne({userName: req.body.userName})
-        const blog = new Blog({
-            user: user._id,
-            title: req.body.title,
-            text: req.body.text,
-            timeStamp: datetime,
-            public: req.body.public
-        })
-        await blog.save()
-        res.json({success: true , blog: blog})
+        const user = await User.findOne({ userName: req.body.userName })
+        if (req.user.userName !== user.userName) {
+            res.json({success: false , msg: "Unauthorized to post with this username"})
+        }
+        else {
+            const currentdate = new Date();
+            const datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth() + 1) + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+            const blog = new Blog({
+                user: user._id,
+                title: req.body.title,
+                text: req.body.text,
+                timeStamp: datetime,
+                public: req.body.public
+            })
+            await blog.save()
+            res.json({ success: true, blog: blog })
+        }
     })
 ]
 
-exports.update = asyncHandler(async (req, res, next) => {
-    res.json("Update : To be implemented")
-})
+exports.update = [
+    body("title")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Not long enough title"),
+    body("text")
+        .trim()
+        .isLength({ min: 100 })
+        .escape()
+        .withMessage("The text content should be more than 100 words"),
+    asyncHandler(async (req, res, next) => {
+        const error = validationResult(req)
+        if (!error.isEmpty()) {
+            return res.json(error)
+        }
+        const blog = await Blog.findOne({ _id : req.params['blogId'] }).populate('user').exec()
+        if (req.user.userName !== blog.user.userName) {
+            res.json({success: false , msg: "Unauthorized to edit"})
+        }
+        else {
+            await Blog.findOneAndUpdate({ _id: req.params['blogId'] }, {
+                title: req.body.title,
+                text: req.body.text,
+                public: req.body.public
+            }, {new: true}).then((blog) => res.json({success: true , blog: blog}))
+        }
+    })
+]
+
 exports.delete = asyncHandler(async (req, res, next) => {
     res.json("Delete : To be implemented")
 })
