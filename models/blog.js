fcb26d64c2f2aeeb2c2e31be42a26aa2ad5bae3d
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-02-19 20:30:48 +0530

    Update the database model of blog and user

--- models/blog.js
+++ models/blog.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema
 
 const blogSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User" },
-    title: { type: String , minlength: 1 , required: true},
-    text: { type: String , minlength: 100 , required: true},
-    timeStamp: { type: String , required: true},
-    public: {type: Boolean, required: true},
+    title: { type: String, minlength: 1, required: true },
+    text: { type: String, minlength: 100, required: true },
+    timeStamp: { type: String, required: true },
+    public: { type: Boolean, required: true },
+    comments: [{ type: Schema.ObjectId, ref: "Comment" }],
 })
 
-module.exports = mongoose.model("Blog" , blogSchema)
+module.exports = mongoose.model("Blog", blogSchema)
--- models/user.js
+++ models/user.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema
 
 
 const userSchema = new Schema({
-    userName: {type: String , required:true , minlength: 1},
-    hash: {type: String , required: true},
+    userName: { type: String, required: true, minlength: 1 },
+    hash: { type: String, required: true },
+    blogs: [{ type: Schema.ObjectId, ref: "Blog" }],
+    comments: [{ type: Schema.ObjectId, ref: "Comment" }],
 })
 
-module.exports = mongoose.model("User" , userSchema)
+module.exports = mongoose.model("User", userSchema)
